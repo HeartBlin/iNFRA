e06add2ef9fa872988174426c2f36dba40b5cb30
HeartBlin <161874560+HeartBlin@users.noreply.github.com>
2026-08-02 00:55:28 +0300

    chore: switch to gh for nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     systems.url = "github:nix-systems/x86_64-linux";
 
     # Declarative partitioning
